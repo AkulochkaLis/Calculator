54ae7c43f12038201a9072e0b10c4bce2e7dcd70
AkulochkaLis <tomaochan@yahoo.com>
2025-01-24 14:40:38 +0300

    refactoring

--- Calc.cs
+++ Calc.cs
@@ -1,27 +1,7 @@
 using Godot;
 using System;
-using System.Data;
-using System.Diagnostics;
-// как работает калькулятор: 
-// 1. получает число
-// 2. запоминает число  как leftNumber
-// 3. выведи leftNumber
-// 4. получает действие
-// 5. запоминает действие  как operator
-// 6. если operator =, то:
-//   6.1. выведи leftNumber
-// 7. если не =, то:
-//   7.1 получает число 
-//   7.2 запоминает число как rightNumber 
-//   7.3 выведи rightNumber 
-//   7.4 получает действие   
-//   7.5 применяет operator к leftNumber и rightNumber
-//   7.6 запоминает как leftNumber 
-//   7.7 запоминает действие  как operator
-//   7.8 если operator не =, то:
-//     7.8.1 перейди к пункту 7 
-//   7.9 если operator =, то:
-//     7.9.1  выводит leftNumber
+
+namespace Calculator;
 
 public partial class Calc : Node2D
 {
@@ -32,29 +12,15 @@ public partial class Calc : Node2D
 	private double rightFractional;
 
 	private string _operator = "";
-	// - данное свойствое описывает состояние кал-ра, где default - стандартное состояние
 	private string state = "default";
-
-	private string numScreen = "";
-
-	private string numScreenR = "";
 
 	private Screen screen;
 
-
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		screen = GetNode<Screen>("/root/Node2D/PanelContainer/Screen");
 	}
 
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-
-	// принимает число, запоминает и выводит его 
 	public void Number(double num)
 	{
 
@@ -104,7 +70,6 @@ public partial class Calc : Node2D
 			state = "default";
 			fractional = 0;
 			rightFractional = 0;
-			numScreen = "";
 			leftNumber = 0;
 			rightNumber = 0;
 			_operator = "";
@@ -198,11 +163,5 @@ public partial class Calc : Node2D
 		rightFractional = 0;
 		rightNumber = 0;
 		state = "rightInput";
-
-		GD.Print(leftNumber);
-		GD.Print(rightNumber);
-		GD.Print(fractional);
-		GD.Print(state);
-
 	}
 }
